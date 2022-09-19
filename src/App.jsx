99a7c7386696d2943e4b17cdc1ae6fb19a81a334
vikas kashyap <vkashyap81098@gmail.com>
2022-09-19 12:56:55 +0530

    created my portfolio again

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Top from "./Top";
+import About from "./About";
+import Skills from "./Skills";
+
+function App() {
+  return (
+    <div>
+      <Top />
+      <About />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <div>
+      <h1>My Skills</h1>
+      <a>hello world</a>
+    </div>
+  );
+}
+
+export default Skills;
